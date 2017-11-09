b8ea8f2006fe5e90ffeb4ff4564cb2b16f116b10
Nick Garcia <89nickgarcia@gmail.com>
2017-11-09 05:08:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const companyName='Scuber';
 
-var mostProfitableNeighborhood='Chelsea';
+let mostProfitableNeighborhood='Chelsea';
 
 let companyCeo='Susan Smith';
